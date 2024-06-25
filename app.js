ba43189975aa4726672f4e61f97cd835e3dac068
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2024-06-25 15:14:59 -0600

    actualizacion ejercicios

--- app.js
+++ app.js
@@ -1,25 +1,84 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Hora del Desafio';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Hora del Desafio';
 
 
-function botonClicado (){
-console.log('El boton fue clicado')
+// function botonClicado (){
+// console.log('El boton fue clicado')
+// }
+
+
+// function botonPrompt(){
+//     let ciudad = prompt('Ingresa el nombre de una ciudad de Mexico: ');
+//     alert(`Estuve en ${ciudad} y me acorde de ti!`)
+// }
+
+// function alerta() {
+//     alert('Yo amo JS')
+// }
+
+// function suma(){
+//     let num1 = parseInt(prompt('Ingresa el numero 1'));
+//     let num2 = parseInt(prompt('ingresa el numero 2'));
+//     let suma = num1 + num2;
+
+//     alert(`El resultado de la suma de ${num1} + ${num2} es  = ${suma}`);
+// }
+
+//Desafio funciones
+
+//Ejercicio 1
+
+function saludo() {
+    console.log('Hola, mundo!')
+};
+
+saludo();
+
+//Ejercicio 2
+
+function saludoNombre(nombre) {
+    console.log(`Hola, ${nombre}!`)
+};
+
+saludoNombre('Mario');
+
+// Ejercicio 3
+
+function duplicar(num) {
+    let doble = num * 2;
+    return doble;
 }
 
+console.log(duplicar(22));
 
-function botonPrompt(){
-    let ciudad = prompt('Ingresa el nombre de una ciudad de Mexico: ');
-    alert(`Estuve en ${ciudad} y me acorde de ti!`)
+//Ejercicio 4
+
+function calcularPromedio(num1, num2, num3){
+    let promedio = (num1 + num2 + num3) / 3;
+
+    return promedio;
 }
 
-function alerta() {
-    alert('Yo amo JS')
+console.log(calcularPromedio(2, 4, 6));
+
+//Ejercicio 5
+
+function numeroMayor(num1, num2) {
+
+    if(num1 > num2) {
+        return `El ${num1} es mayor que ${num2}`
+    } else {
+        return `El ${num2} es mayor que ${num1}`
+    }
 }
 
-function suma(){
-    let num1 = parseInt(prompt('Ingresa el numero 1'));
-    let num2 = parseInt(prompt('ingresa el numero 2'));
-    let suma = num1 + num2;
+console.log(numeroMayor(1,2));
 
-    alert(`El resultado de la suma de ${num1} + ${num2} es  = ${suma}`);
+//Ejercicio 6
+
+function multiplicador (num1) {
+    let doble = (num1*num1)
+    return doble;
 }
+
+console.log(multiplicador(6));
