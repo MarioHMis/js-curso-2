76ddea77dc6c7e7995656f5f85c368defa93e59a
mariohmis <138615425+MarioHMis@users.noreply.github.com>
2024-06-26 13:14:45 -0600

    Desafio actualizado

--- app.js
+++ app.js
@@ -28,57 +28,122 @@
 
 //Ejercicio 1
 
-function saludo() {
-    console.log('Hola, mundo!')
-};
+// function saludo() {
+//     console.log('Hola, mundo!')
+// };
 
-saludo();
+// saludo();
 
-//Ejercicio 2
+// //Ejercicio 2
 
-function saludoNombre(nombre) {
-    console.log(`Hola, ${nombre}!`)
-};
+// function saludoNombre(nombre) {
+//     console.log(`Hola, ${nombre}!`)
+// };
 
-saludoNombre('Mario');
+// saludoNombre('Mario');
 
-// Ejercicio 3
+// // Ejercicio 3
 
-function duplicar(num) {
-    let doble = num * 2;
-    return doble;
-}
+// function duplicar(num) {
+//     let doble = num * 2;
+//     return doble;
+// }
 
-console.log(duplicar(22));
+// console.log(duplicar(22));
 
-//Ejercicio 4
+// //Ejercicio 4
 
-function calcularPromedio(num1, num2, num3){
-    let promedio = (num1 + num2 + num3) / 3;
+// function calcularPromedio(num1, num2, num3){
+//     let promedio = (num1 + num2 + num3) / 3;
 
-    return promedio;
-}
+//     return promedio;
+// }
 
-console.log(calcularPromedio(2, 4, 6));
+// console.log(calcularPromedio(2, 4, 6));
 
-//Ejercicio 5
+// //Ejercicio 5
 
-function numeroMayor(num1, num2) {
+// function numeroMayor(num1, num2) {
 
-    if(num1 > num2) {
-        return `El ${num1} es mayor que ${num2}`
-    } else {
-        return `El ${num2} es mayor que ${num1}`
-    }
-}
+//     if(num1 > num2) {
+//         return `El ${num1} es mayor que ${num2}`
+//     } else {
+//         return `El ${num2} es mayor que ${num1}`
+//     }
+// }
 
-console.log(numeroMayor(1,2));
+// console.log(numeroMayor(1,2));
+
+// //Ejercicio 6
+
+// function multiplicador (num1) {
+//     let doble = (num1*num1)
+//     return doble;
+// }
+
+// console.log(multiplicador(6));
+
+//Ejercicio 1
+
+// function indiceMasaCorporal(altura, peso) {
+// let resultado = parseFloat(peso / (altura * altura));
+// return resultado;
+// };
+
+// console.log(indiceMasaCorporal(1.76, 87 ));
+
+// //Ejercicio 2
+
+// function factorial(n) {
+//     let result = 1;
+//     for (let i = 2; i <= n; i++) {
+//       result *= i;
+//     }
+//     return result;
+//   }
+  
+// console.log(factorial(5));
+
+// //Ejercio 3
+
+// function conversionMoneda(dolar) {
+//     let real = 4.8;
+//    conversion = parseFloat(dolar * real)
+//    return conversion;
+// }
+
+// console.log(conversionMoneda(10));
+
+// // Ejercicio 4
+
+// function areaRectangular(altura, anchura) {
+//     areaSala = (altura * anchura)
+//     return alert(areaSala);
+// }
+
+// area(2, 6)
+
+// // Ejercicio 5
+
+// function areaCirculo(radio){
+//    let areaCircular = Math.PI * (radio * radio);
+//     return areaCircular;
+// }
+
+// console.log(areaCirculo(3));
 
 //Ejercicio 6
 
-function multiplicador (num1) {
-    let doble = (num1*num1)
-    return doble;
-}
+// function tablaDelNumero(numero) {
 
-console.log(multiplicador(6));
+//   let resultado = '';
+//   for (let i = 1; i <= 10; i++) {
+//     resultado += `${numero} x ${i} = ${numero * i}\n`
+//   }
+//   return resultado;
+
+    
+// }
+
+
+// console.log(tablaDelNumero(5));
